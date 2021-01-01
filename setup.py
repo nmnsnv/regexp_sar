8cd9a0c5fd7b4072fcfab902fad5b33d4de5528c
noam <noam.nisanov@gmail.com>
2021-01-01 22:32:26 +0200

    Support py 3.4 and above, removed test package (nose) dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def main():
     with open("README.md", "r", encoding="utf-8") as fh:
         long_description = fh.read()
     setup(name="regexp-sar",
-          version="0.1.0b1",
+          version="0.1.0b3",
 
           description="SAR Regexp Engine for Python",
           long_description=long_description,
@@ -16,15 +16,15 @@ def main():
           author="Noam Nisanov",
           author_email="noam.nisanov@gmail.com",
 
-          test_suite='nose.collector',
-          tests_require=['nose'],
+          # test_suite='nose.collector',
+          # tests_require=['nose'],
 
           ext_modules=[Extension("regexp_sar._regexp_sar_c",
                                  ["sarmodule.c", "sarcore.c"],
                                  py_limited_api=True)],
           headers=['sarcore.h'],
           license="MIT",
-          python_requires='>=35',
+          python_requires='>=3.4',
           packages=["regexp_sar"])
 
 
